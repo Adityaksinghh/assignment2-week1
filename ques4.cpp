61959a038cd72187fdc3ab53c9b76fe5ab9d14b1
Adityaksinghh <152866092+Adityaksinghh@users.noreply.github.com>
2024-02-16 18:05:22 +0530

    Add files via upload

---
+++ ques4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,h;//taking r=radius and h=height
+    cout<<"enter radius: ";
+    cin>>r;
+    cout<<"enter height: ";
+    cin>>h;
+    float volume;//volume = volume of cylinder
+    volume=3.1415*r*r*h;
+    cout<<"volume of cylinder= "<<volume;
+
+}
---
+++ ques5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x,y;
+    cout<<"enter the first character: "<<endl;
+    cin>>x;
+    cout<<"enter the second character: "<<endl;
+    cin>>y;
+    cout<<"the diff of ASCII value is: "<<x-y<<endl;
+
+}
